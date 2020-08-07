021053f9f0cc2b9e2239be8232e5f22f2685c1bf
cseibert2667 <cseibert2667@gmail.com>
2020-08-07 14:44:23 -0700

    styling changes, outline for js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // GIVEN I am taking a code quiz
+
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
+// Hide start prompt (display: none)
+// show questions prompt (display: block)
+// populate questions prompt by pulling them from an array of objects
+
+
 // WHEN I answer a question
 // THEN I am presented with another question
+// for loop to pass in questions and button values until there are none left
+// make sure to clear out previous button values when moving to next question
+
+
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
+
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
+// display final score, prompt to start over
+
+
+// BONUS
 // WHEN the game is over
 // THEN I can save my initials and score
+
+
+
