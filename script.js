60332bfbc1a8bb98e38f810079794dde4a65d277
cseibert2667 <cseibert2667@gmail.com>
2020-08-09 19:55:02 -0700

    got it checking answers but not sure how to move to next page

--- script.js
+++ script.js
@@ -10,58 +10,91 @@ var option2 = document.querySelector("#option2")
 var option3 = document.querySelector("#option3")
 var option4 = document.querySelector("#option4")
 var startingTime = 100
+var questionIndex = 0
 
 // Questions & Answers
 var questions = [
-    {q: "This is where we ask the first question?", o: ["cake", "bunny", "stick", "lettuce"], a: "lettuce"},
-    {q: "This is where we ask the second question?", o: ["option1", "option2", "option3", "option4"], a: "option3"},
-    {q: "This is where we ask the third question?", o: ["option1", "option2", "option3", "option4"], a: "option3"},
-    {q: "This is where we ask the fourth question?", o: ["option1", "option2", "option3", "option4"], a: "chunky"},
-    {q: "This is where we ask the fifth question?", o: ["option1", "option2", "option3", "option4"], a: "option3"},
+    { q: "This is where we ask the first question?", o: ["cake", "bunny", "stick", "lettuce"], a: "bunny" },
+    { q: "This is where we ask the second question?", o: ["option0", "option2", "option3", "option4"], a: "option0" },
+    { q: "This is where we ask the third question?", o: ["option1", "option2", "option3", "option4"], a: "option3" },
+    { q: "This is where we ask the fourth question?", o: ["chunky", "option2", "option3", "option4"], a: "chunky" },
+    { q: "This is where we ask the fifth question?", o: ["option1", "option2", "option3", "option4"], a: "option4" },
 ]
-
+// debugger;
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-startButton.addEventListener("click", function(){
-    startScreen.style.display="none";  // Hides start prompt
-    qScreen.style.display="block";  // show questions prompt
+startButton.addEventListener("click", function () {
+    startScreen.style.display = "none";  // Hides start prompt
+    qScreen.style.display = "block";  // show questions prompt
     setTime() // starts timer
-    showQuestion(0)
+
+    showQuestion(questionIndex);
+    checkAnswer(questionIndex);
 })
 
 
 function setTime() {
-    var timerInterval = setInterval(function() {
-      startingTime--;
-      timer.textContent = "Score: " + startingTime;  // Displays timer
-  
-      if(startingTime === 0) {
-        clearInterval(timerInterval);
-        console.log("times up");  // What happens when time runs out
-      }
-  
+    var timerInterval = setInterval(function () {
+        startingTime--;
+        timer.textContent = "Score: " + startingTime;  // Displays timer
+
+        if (startingTime === 0) {
+            clearInterval(timerInterval);
+            console.log("times up");  // What happens when time runs out
+        }
+
     }, 1000);
-  }
-  
+}
+
 // populate questions prompt by pulling them from an array of objects
-function showQuestion(i){
+function showQuestion(i) {
     var question = questions[i].q;
     var options = questions[i].o;
     questionText.textContent = question;
-    for (let n = 0; n < options.length; n++) {
+    for (n = 0; n < options.length; n++) {
         var btn = document.createElement("a");
         btn.setAttribute("class", "btn btn-primary");
+        btn.setAttribute("value", options[n])
         btn.textContent = options[n];
         choices.appendChild(btn);
     }
-    // option2.textContent = options[1]
-    // option3.textContent = options[2]
-    // option4.textContent = options[3]
 }
+
+
+function checkAnswer(i) {
+    choices.addEventListener("click", function (e) {
+        btnValue = e.target;
+        selectedAnswer = btnValue.getAttribute("value");
+
+        if (selectedAnswer === null) { return } // do nothing if button is not clicked
+        else if (selectedAnswer === questions[i].a) {
+            console.log("correct");
+
+            //correct
+        } else {
+            console.log("incorrect")
+
+            //incorrect
+
+        }
+    }
+    )
+}
+
+
+function resetQuestion() {
+    while (choices.firstChild) {
+        choices.removeChild(choices.firstChild)
+    }
+}
+
+
+
+
+
 // WHEN I answer a question
 // THEN I am presented with another question
-
 // for loop to pass in questions and button values until there are none left
 // make sure to clear out previous button values when moving to next question
 
